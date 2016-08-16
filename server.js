fbc36b5690f0e0ce3a542e9b4a0261052525da67
Mark Jones <mark@getkahoot.com>
2016-08-16 15:48:37 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+
+var player;
+var host;
+
+function connectPlayer() {
+	console.log('player connected');
+	player = this;
+}
+
+function connectHost() {
+	console.log('host connected');
+	host = this;
+}
+
+io.on('connection', function(socket){
+	socket.on('connectPlayer', connectPlayer);
+	socket.on('connectHost', connectHost);
+
+	socket.on('nextQuestion', function() {
+		player.emit('nextQuestion');
+	});
+
+	socket.on('showImage', function() {
+		player.emit('showImage');
+	});
+});
+
+server.listen(3000);
